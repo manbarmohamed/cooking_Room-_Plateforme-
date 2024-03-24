3de5e6b20e5496cf073458094263a80742282a63
YassineOularbi <yassineoularbi4@gmail.com>
2024-03-25 00:38:24 +0100

    Last modifications

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// let card = document.getElementById("cont-card");
-
-// for (let i = 0; i < ArryRecipes.length; i++) {
-//     let cardDiv = document.createElement('div');
-//     cardDiv.classList.add('col');
-//     cardDiv.innerHTML = `
-//     <div class="card d-flex justify-content-center align-items-center mt-5">
-//     <img src="${ArryRecipes[i].photo}" alt="" class="card-img rounded-circle w-50">
-//     <div class="card-body d-flex justify-content-center align-items-center flex-column">
-
-//         <h4 class="card-title">${ArryRecipes[i].title2}</h4>
-//             <p class="card-text"> ${ArryRecipes[i].descreption}</p>
-//             <div class="feedback">
-//                 <p><i class="fa-regular fa-heart text-start like-click" onclick="likesCount(this)"> <span class="like-count">0</span></i></p>
-//                 <p><i class="fa-regular fa-comment like-click" onclick="commentsCount(this)"> <span class="like-count">0</span></i></p>
-//             </div>
-
-//     </div>
-// </div>`;
-//     card.appendChild(cardDiv);
-// }
-
-// function likesCount(element){
-//     let likeCountSpan = element.querySelector('.like-count');
-//     let currentLikes = parseInt(likeCountSpan.textContent);
-//     likeCountSpan.textContent = currentLikes + 1;
-// }
-
-// function commentsCount(element){
-//     let commentCountSpan = element.querySelector('.like-count');
-//     let currentComments = parseInt(commentCountSpan.textContent);
-//     commentCountSpan.textContent = currentComments + 1;
-// }
-
 let bigImage=document.getElementById('big-img');
 
 function myDish(dish) {
@@ -75,84 +41,145 @@ function delicious(){
     desc3.innerText="Experience pure gastronomic joy with every bite."
 }
 
-// let ArryRecipes = [
-//     {
-//         photo: "images/images/dish/2.png",
-//         // title : "Cream prawen,inspired by morocco",
-//         title2: "salade Perry",
-//         descreption: "Salade nesoiase test de ca cord",
-//         likes: 30,
-//         comments: 10,
-//     },
-//     {
-//         photo: "images/images/dish/1.png",
-//         // title : "Cream prawen,inspired by morocco",
-//         title2: "salade Maroccan",
-//         descreption: "Salade nesoiase test de ca cord",
-//         likes: 3,
-//         comments: 90,
-//     },
-// ];
-
 let ArryRecipes = [
-    {
-        photo: "images/images/dish/2.png",
-        title: "salade Perry",
-        description: "Salade herry test de ca cord",
-        likes: 30,
-        comments: 10,
-    },
-    {
-        photo: "images/images/dish/1.png",
-        title: "salade Maroccan",
-        description: "Salade nesoiase test de ca cord",
-        likes: 3,
-        comments: 90,
-    },
-    {
-        photo: "images/images/dish/1.png",
-        title: "salade Maroccan",
-        description: "Salade nesoiase test de ca cord",
-        likes: 3,
-        comments: 90,
-    },
-    {
-        photo: "images/images/dish/1.png",
-        title: "salade Maroccan",
-        description: "Salade nesoiase test de ca cord",
-        likes: 3,
-        comments: 90,
-    },
-    {
-        photo: "images/images/dish/1.png",
-        title: "salade Maroccan",
-        description: "Salade nesoiase test de ca cord",
-        likes: 3,
-        comments: 90,
-    },
-    {
-        photo: "images/images/dish/1.png",
-        title: "salade Maroccan",
-        description: "Salade nesoiase test de ca cord",
-        likes: 3,
-        comments: 90,
-    },
+  {
+    photo: "images/images/dish/1.png",
+    title: "salade Perry",
+    description: "Salade nesoiase test de ca cord",
+    likes: 30,
+    comments: 10,
+    categorie: "Salad",
+    ingredient: "-pomme de terre -salamon",
+    direction: "PREHEAT A LARGE HEAVY SKILLET OVER MEDIUM HEAT FOR 3 MINUTES",
+    time: "-10min preparation time -10min cooking time",
+    serving: "this specific recipe serves up to four people. ",
+    nat_value: "-100kcal -25.6g -23g ",
+  },
+  {
+    photo: "images/images/dish/2.png",
+    title: "salade Maroccan",
+    description: "Salade nesoiase test de ca cord",
+    likes: 3,
+    comments: 90,
+    categorie: "Salad",
+    ingredient: "tomate -pomme de terre -salamon",
+    direction: "PREHEAT A LARGE HEAVY SKILLET OVER MEDIUM HEAT FOR 3 MINUTES",
+    time: "-10min preparation time -10min cooking time",
+    serving: "this specific recipe serves up to four people. ",
+    nat_value: "-100kcal -25.6g -23g ",
+  },
+  {
+    photo: "images/images/dish/3.png",
+    title: "salade Maroccan",
+    description: "Salade nesoiase test de ca cord",
+    likes: 3,
+    comments: 90,
+    categorie: "Fast Food",
+    ingredient: "tomate -pomme de terre -salamon",
+    direction: "PREHEAT A LARGE HEAVY SKILLET OVER MEDIUM HEAT FOR 3 MINUTES",
+    time: "-10min preparation time -10min cooking time",
+    serving: "this specific recipe serves up to four people. ",
+    nat_value: "-100kcal -25.6g -23g ",
+  },
+  {
+    photo: "images/images/dish/4.png",
+    title: "salade Maroccan",
+    description: "Salade nesoiase test de ca cord",
+    likes: 3,
+    comments: 90,
+    categorie: "Burger",
+    ingredient: "tomate -pomme de terre -salamon",
+    direction: "PREHEAT A LARGE HEAVY SKILLET OVER MEDIUM HEAT FOR 3 MINUTES",
+    time: "-10min preparation time -10min cooking time",
+    serving: "this specific recipe serves up to four people. ",
+    nat_value: "-100kcal -25.6g -23g ",
+  },
+  {
+    photo: "images/images/dish/5.png",
+    title: "salade Maroccan",
+    description: "Salade nesoiase test de ca cord",
+    likes: 3,
+    comments: 90,
+    categorie: "Salad",
+    ingredient: "tomate -pomme de terre -salamon",
+    direction: "PREHEAT A LARGE HEAVY SKILLET OVER MEDIUM HEAT FOR 3 MINUTES",
+    time: "-10min preparation time -10min cooking time",
+    serving: "this specific recipe serves up to four people. ",
+    nat_value: "-100kcal -25.6g -23g ",
+  },
+  {
+    photo: "images/images/dish/6.png",
+    title: "salade Maroccan",
+    description: "Salade nesoiase test de ca cord",
+    likes: 3,
+    comments: 90,
+    categorie: "Burger",
+    ingredient: "tomate -pomme de terre -salamon",
+    direction: "PREHEAT A LARGE HEAVY SKILLET OVER MEDIUM HEAT FOR 3 MINUTES",
+    time: "-10min preparation time -10min cooking time",
+    serving: "this specific recipe serves up to four people. ",
+    nat_value: "-100kcal -25.6g -23g ",
+  },
 ];
-function getCard(){
+function getCard(recipes){
 
 let cardHTML = "";
 
-for (let i = 0; i < ArryRecipes.length; i++) {
+for (let i = 0; i < recipes.length; i++) {
     cardHTML += `
     <div class="col card-search" data-index="${i}">
         <div class="card d-flex justify-content-center align-items-center mt-5">
-            <img src="${ArryRecipes[i].photo}" alt="" class="card-img rounded-circle w-50">
-            <div class="card-body d-flex justify-content-center align-items-center flex-column">
-                <h4  class="card-title">${ArryRecipes[i].title}</h4>
-                <p class="card-text">${ArryRecipes[i].description}</p>
+            <img onclick="ingredient(this.src)" src="${recipes[i].photo}" alt="" class="card-img rounded-circle w-75">
+            <div class="card-body d-flex justify-content-center align-items-center flex-column" style="width: 80%;">
+                <h4 class="card-title">${recipes[i].title}</h4>
+                <p class="card-text">${recipes[i].description}</p>
                 <div class="feedback">
-                    <p><i class="fa-regular fa-heart text-start like-click" onclick="likesCount(this)"> <span class="like-count">0</span></i></p>
-                    <p><i class="fa-regular fa-comment like-click" onclick="commentsCount(this)"> <span class="comment-count">0</span></i></p>
+    <p><i class="fa-regular fa-heart text-start like-click" onclick="likesCount(this)"> <span class="like-count">${recipes[i].likes}</span></i></p>
+                <p type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal"
+  data-bs-whatever="@getbootstrap" id="add-recipe"><i class="fa-regular fa-comment like-click"> <span id="comment-count-1" class="comment-count">${recipes[i].comments}</span></i>
+</p>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Add Recipe</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+
+            <div class="container-rate">
+              <div class="post">
+                <div class="text">Thanks for rating us!</div>
+                <div class="edit">EDIT</div>
+              </div>
+              <div class="star-widget">
+                <input type="radio" name="rate" id="rate-5">
+                <label for="rate-5" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-4">
+                <label for="rate-4" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-3">
+                <label for="rate-3" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-2">
+                <label for="rate-2" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-1">
+                <label for="rate-1" class="fas fa-star"></label>
+                <form class="form-rate" action="#">
+                  <header></header>
+                  <div class="textarea">
+                    <textarea cols="30" placeholder="Describe your experience.."></textarea>
+                  </div>
+                  <div class="btn">
+                    <button type="button" onclick="commentsCount(event, this)" id="submit">Post</button>
+
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
                 </div>
             </div>
         </div>
@@ -164,7 +191,7 @@ let cardElements = document.querySelectorAll('.card-search');
         ArryRecipes[index].element = element;
     });
 }
-getCard()
+//getCard()
 
 
 let search = document.getElementById("search");
@@ -176,12 +203,6 @@ search.addEventListener("input", e => {
         card.element.classList.toggle("hide", !found)
     });
 });
-
-function likesCount(element){
-    let likeCountSpan = element.querySelector('.like-count');
-    let currentLikes = parseInt(likeCountSpan.textContent);
-    likeCountSpan.textContent = currentLikes + 1;
-}
 
 /*let search = document.getElementById("search");
 
@@ -199,3 +220,261 @@ search.addEventListener("input", e => {
         }
     });
 });*/
+function likesCount(element) {
+  let likeCountSpan = element.querySelector(".like-count");
+  let currentLikes = parseInt(likeCountSpan.textContent);
+  likeCountSpan.textContent = currentLikes + 1;
+}
+function commentsCount(event, buttonElement) {
+    event.preventDefault(); 
+    let commentCountElement = document.getElementById('comment-count-1');
+    if (commentCountElement) {
+        let currentComments = parseInt(commentCountElement.textContent);        
+        let newComments = currentComments + 1;
+        commentCountElement.textContent = newComments;
+    } 
+}
+
+
+
+/*function commentsCount(element) {
+        let commentCountSpan = element.querySelector('.comment-count');
+        let currentComments = parseInt(commentCountSpan.textContent);
+        commentCountSpan.textContent = currentComments + 1;
+}*/
+
+
+let submit=document.getElementById("submit")
+let img = document.getElementById("img-recette")
+let titlerecette = document.getElementById("title-recette")
+let descriptionR = document.getElementById("descr-recette")
+let cate = document.getElementById("cat")
+let inger = document.getElementById("ingr");
+let direc = document.getElementById("direc");
+let time = document.getElementById("time");
+let serv = document.getElementById("serv");
+let nt_value = document.getElementById("nt-value");
+
+
+
+
+
+
+
+function addCards() {
+  const card = {
+    photo: img.files.length > 0 ? URL.createObjectURL(img.files[0]) : "",
+    title: titlerecette.value,
+    description: descriptionR.value,
+    likes: 0,
+    comments: 0,
+    categorie: cate.value,
+    ingredient: inger.value,
+    direction: direc.value,
+    time: time.value,
+    serving: serv.value,
+    nat_value: nt_value.value,
+  };
+  ArryRecipes.push(card);
+}
+
+document.getElementById("send").onclick = function () {
+    // document.getElementById("cont-card").innerHTML ="";
+
+    img.onchange = function () {
+        img.src = URL.createObjectURL(img.files[0])
+    }
+
+    addCards();
+    paginationChange();
+}
+
+
+
+
+//pagination
+
+
+
+function paginate(array, page_size, page_number) {
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+}
+
+// Fonction pour afficher les recettes
+function paginationChange() {
+    document.addEventListener("DOMContentLoaded", function () {
+        const PAGE_SIZE = 3;
+        let currentPage = 1;
+        getCard(paginate(ArryRecipes, PAGE_SIZE, currentPage))
+        // console.log(currentPage);
+        const paginationLinks = document.querySelectorAll(".page-link");
+        paginationLinks.forEach(link => {
+            link.addEventListener("click", function (e) {
+                e.preventDefault();
+                const pageNumber = parseInt(e.target.innerText);
+                if (!isNaN(pageNumber)) {
+                    currentPage = pageNumber;
+                    getCard(paginate(ArryRecipes, PAGE_SIZE, currentPage));
+                }
+            });
+        });
+
+        document.getElementById("previous-button").addEventListener("click", function (e) {
+            e.preventDefault();
+            if (currentPage > 1) {
+                currentPage--;
+                getCard(paginate(ArryRecipes, PAGE_SIZE, currentPage));
+            }
+        });
+
+        document.getElementById("next-button").addEventListener("click", function (e) {
+            e.preventDefault();
+            const totalPages = Math.ceil(ArryRecipes.length / PAGE_SIZE);
+            if (currentPage < totalPages) {
+                currentPage++;
+                getCard(paginate(ArryRecipes, PAGE_SIZE, currentPage));
+            }
+        });
+
+    });
+
+}
+paginationChange()
+
+
+function filtreByCategorie(categore) {
+    let filtreCard = ArryRecipes.filter(Arr => Arr.categorie === categore)
+    getCard(filtreCard)
+}
+
+const catlink = document.querySelectorAll(".categorie")
+catlink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const cate = e.target.dataset.category;
+        filtreByCategorie(cate)
+    })
+})
+
+
+//Rating
+
+
+const btn = document.querySelector("button");
+const post = document.querySelector(".post");
+const widget = document.querySelector(".star-widget");
+const editBtn = document.querySelector(".edit");
+btn.onclick = () => {
+    widget.style.display = "none";
+    post.style.display = "block";
+    editBtn.onclick = () => {
+        widget.style.display = "block";
+        post.style.display = "none";
+    }
+    return false;
+}
+// ------------popuuuuuuuuuuuuuuuuuuuuuuuup---------------
+// Fonction pour afficher les détails du produit dans la modale
+function showProductDetails(
+  ingredientsTab,
+  directionsTab,
+  timeTab,
+  servingTab,
+  nutritionTab
+) {
+  for (let j = 0; j < ArryRecipes.length; j++) {
+    // Mettre à jour les éléments de la modale avec les détails du produit
+    ingredientsTab.innerHTML = `
+      <h3>Ingredients</h3>
+      <p>${ArryRecipes[j].ingredient} ${ArryRecipes[j].direction}</p>
+    `;
+
+    directionsTab.innerHTML = `
+      <h3>Directions</h3>
+      <p>
+        ${ArryRecipes[j].direction}
+      </p>
+    `;
+
+    timeTab.innerHTML = `
+      <h3>Estimated Time</h3>
+      <p>${ArryRecipes[j].time}</p>
+    `;
+
+    servingTab.innerHTML = `
+      <h3>Servings</h3>
+      <p>${ArryRecipes[j].serving}</p>
+    `;
+
+    nutritionTab.innerHTML = `
+      <h3>Nutritional Value</h3>
+      <p><i>Based on a 2000 calorie diet</i></p>
+      <p>${ArryRecipes[j].nat_value}</p>
+    `;
+  }
+  // Afficher la modale
+  let productModal = document.getElementById("productModal");
+  if (productModal) {
+    productModal.classList.add("show");
+  }
+}
+
+function ingredient(img) {
+  // showProductDetails()
+  let ingredientsTab = document.getElementById("ingredients");
+  let directionsTab = document.getElementById("directions");
+  let timeTab = document.getElementById("time");
+  let servingTab = document.getElementById("serving");
+  let nutritionTab = document.getElementById("nutrition");
+  // Afficher les détails du produit dans la modale
+  showProductDetails(
+    ingredientsTab,
+    directionsTab,
+    timeTab,
+    servingTab,
+    nutritionTab
+  );
+}
+
+
+// Ajouter un gestionnaire d'événement de clic pour chaque colonne
+// let columns = document.querySelectorAll("#card-img");
+
+// columns.forEach(column => {
+//   column.addEventListener("click", function () {
+//     console.log("ffggg")
+//     // Récupérer les détails du produit à partir des attributs data-* de la colonne
+//     let ingredientsTab = document.getElementById("ingredients");
+//     let directionsTab = document.getElementById("directions");
+//     let timeTab = document.getElementById("time");
+//     let servingTab = document.getElementById("serving");
+//     let nutritionTab = document.getElementById("nutrition");
+
+//     // Afficher les détails du produit dans la modale
+//     showProductDetails(
+//       ingredientsTab,
+//       directionsTab,
+//       timeTab,
+//       servingTab,
+//       nutritionTab
+//     );
+//   });
+// });
+// -----------------
+function salmon(evt, recipe) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(recipe).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// ----------- end popuuuuuuuuuuuuuuuuuuuuuup-------
